5e432c872d3e7020b426b53b55668ad09e5e2439
edischin <7819978+edischin@users.noreply.github.com>
2020-08-09 17:57:50 -0300

    teacher list and favorites pages (mobile)

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOulineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
 
@@ -20,6 +25,31 @@ const TeacherItem:React.FC<TeacherItemProps> = ({ title }) => {
         <View style={styles.profileInfo}>
           <Text style={styles.name}>Edwin Dischinger</Text>
           <Text style={styles.subject}>Física</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Entusiasta das melhores tecnologias de química avançada.
+        {'\n\n'} Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/*<Image source={heartOulineIcon} />*/}
+            <Image source={unfavoriteIcon} />
+          </RectButton>
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>
+              Entrar em contato
+            </Text>
+          </RectButton>
         </View>
       </View>
     </View>
