0eabb853e00e8f7c0918582df8f7eea5b68646a9
edischin <7819978+edischin@users.noreply.github.com>
2020-08-08 21:49:38 -0300

    From states; Web connection to Server

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,32 +1,51 @@
 import React from 'react';
+import api from '../../services/api';
 
 import './styles.css';
 
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  cost: number;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', { user_id: teacher.id });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/95136?s=460&u=e5adcee574a8067fefde24fbc1e78b2eab7f9378&v=4" alt="David Fowler" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>David Fowler</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /><br />Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-      </p>
+      
+      <p>{teacher.bio}</p>
+
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a href={`https://wa.me/${teacher.whatsapp}`} target="_blank"
+          onClick={createNewConnection} >
           <img src={ whatsAppIcon } alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
